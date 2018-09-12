f1b731805a4d025aca0ffffb98bc8d3903d96d9c
Travis CI User <travis@example.org>
2018-09-12 09:23:24 +0000

    Update documentation

---
+++ implementors/serde/de/trait.Deserialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web3"] = [{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'a&gt; for <a class=\"struct\" href=\"web3/types/struct.Bytes.html\" title=\"struct web3::types::Bytes\">Bytes</a>",synthetic:false,types:["web3::types::bytes::Bytes"]},{text:"impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"enum\" href=\"web3/types/enum.SyncState.html\" title=\"enum web3::types::SyncState\">SyncState</a>",synthetic:false,types:["web3::types::sync_state::SyncState"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'a&gt; for <a class=\"struct\" href=\"web3/types/struct.Work.html\" title=\"struct web3::types::Work\">Work</a>",synthetic:false,types:["web3::types::work::Work"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/serde/ser/trait.Serialize.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["web3"] = [{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"web3/types/enum.BlockNumber.html\" title=\"enum web3::types::BlockNumber\">BlockNumber</a>",synthetic:false,types:["web3::types::block::BlockNumber"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"web3/types/enum.BlockId.html\" title=\"enum web3::types::BlockId\">BlockId</a>",synthetic:false,types:["web3::types::block::BlockId"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"web3/types/struct.Bytes.html\" title=\"struct web3::types::Bytes\">Bytes</a>",synthetic:false,types:["web3::types::bytes::Bytes"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"enum\" href=\"web3/types/enum.SyncState.html\" title=\"enum web3::types::SyncState\">SyncState</a>",synthetic:false,types:["web3::types::sync_state::SyncState"]},{text:"impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.78/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"web3/types/struct.Work.html\" title=\"struct web3::types::Work\">Work</a>",synthetic:false,types:["web3::types::work::Work"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
